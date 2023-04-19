907195724eed2d4a2b5d5bb21fe1657f93052cbb
Long Hoang <79366791+HeyLongHoang@users.noreply.github.com>
2023-04-19 17:37:56 +0700

    Add sample program to move robot

---
+++ autonomous_navigation/simple_move.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+right_motor = Motor(Port.B)
+left_motor = Motor(Port.A)
+
+# # Write your program here.
+ev3.speaker.beep()
+# right_motor.run_time(speed=500, time=5000)
+# left_motor.run_time(speed=500, time=5000)
+
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+robot.straight(1000)
+
+robot.straight(-1000)
+ev3.speaker.beep()
+
+# Turn clockwise by 360 degrees and back again.
+robot.turn(360)
+ev3.speaker.beep()
+
+robot.turn(-360)
+ev3.speaker.beep()
+
