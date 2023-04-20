0c0dd7167c2de19eecb5401582d542c2bca02e74
Darian Krummrich <d.krummrich@gmx.net>
2023-04-20 19:03:39 +0200

    upload of Fatamas UI prototype

--- autonomous_navigation/call_a_star_algo.py
+++ autonomous_navigation/call_a_star_algo.py
@@ -1,18 +1,18 @@
 import subprocess
-from PIL import Image
-from numpy import asarray
+# from PIL import Image
+# from numpy import asarray
 
 
 # read map to pass to the a star algorithm
-image = Image.open('autonomous_navigation/obstacle_course_images/map_1.png')
+# image = Image.open('autonomous_navigation/obstacle_course_images/map_1.png')
 
 # convert to numpy array for iteration over image
-numpydata = asarray(image)
+# numpydata = asarray(image)
 
 # shape
-print(numpydata.shape)
+# print(numpydata.shape)
 
 # data
-print(numpydata)
+# print(numpydata)
 
-# subprocess.call(" python Implementation-of-A-Algorithm-Visualization-via-Pyp5js-\AStar.py -c 25 -r 25 -s 1 -q 3 -e 23 -t 21 -l True", shell=True)
+subprocess.call(" python Implementation-of-A-Algorithm-Visualization-via-Pyp5js-\AStar.py -c 25 -r 25 -s 1 -q 3 -e 23 -t 21 -l True", shell=True)
