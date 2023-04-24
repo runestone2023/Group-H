7ba710b49b71eaa189b16623f4c1f5807e92a46d
Darian Krummrich <d.krummrich@gmx.net>
2023-04-24 18:08:29 +0200

    reads Astar output into main file

--- autonomous_navigation/call_a_star_algo.py
+++ autonomous_navigation/call_a_star_algo.py
@@ -1,20 +1,54 @@
 import subprocess
+import os
+import time
+import csv
 # from PIL import Image
 # from numpy import asarray
+   
 
 
-# read map to pass to the a star algorithm
-# image = Image.open('autonomous_navigation/obstacle_course_images/map_1.png')
+if __name__ == "__main__":
+    # read map to pass to the a star algorithm
+    # image = Image.open('autonomous_navigation/obstacle_course_images/map_1.png')
 
-# convert to numpy array for iteration over image
-# numpydata = asarray(image)
+    # convert to numpy array for iteration over image
+    # numpydata = asarray(image)
 
-# shape
-# print(numpydata.shape)
+    # shape
+    # print(numpydata.shape)
 
-# data
-# print(numpydata)
+    # data
+    # print(numpydata)
 
-# subprocess.call(" python Implementation-of-A-Algorithm-Visualization-via-Pyp5js-\AStar.py -c 25 -r 25 -s 1 -q 3 -e 23 -t 21 -l True", shell=True)
+    # --------------------------------- execute algo ---------------------------------------------------
 
-subprocess.call(" python A_star_algo\AStar.py -c 25 -r 25 -s 1 -q 3 -e 23 -t 21 -l True", shell=True)
+    subprocess.call(" python A_star_algo\AStar.py -c 25 -r 25 -s 1 -q 3 -e 23 -t 21 -l True", shell=True)
+
+
+    # ------------------------ transform output into robot movements ------------------------------------
+
+    # wait until output file is there
+    path_file = 'found_path.csv'
+    
+    while not os.path.exists(path_file):
+        time.sleep(0.1)
+
+    if os.path.isfile(path_file):
+        # read file
+        print('reading file... \n')
+        with open(path_file, newline='') as csvfile:
+
+            pathreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            count = sum(1 for _ in pathreader)
+            print('length is ' + str(count))
+
+            csvfile.seek(0)
+            pathreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in pathreader:
+                print('x:' + row[0] + ' y:' + row[1])
+            
+            # transform it into a difference output
+    else:
+        raise ValueError("%s isn't a file!" % path_file)
+
+
