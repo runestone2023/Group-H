17fb9593075e589dd80908f0f67d4a16e2033545
Long Hoang <79366791+HeyLongHoang@users.noreply.github.com>
2023-05-16 16:37:00 +0700

    Update main.py to correct commands from output

--- autonomous_navigation/output2commands/main.py
+++ autonomous_navigation/output2commands/main.py
@@ -3,80 +3,102 @@ Program to run the robot based on the output of A-star algorithm
 Need to convert the output from type .csv to type .json
 '''
 
+#!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
+
+import json
 # from robot import *
 from output2commands.robot import *
-import json
-import csv
-
 
 ev3 = EV3Brick()
-robot = Robot(l_motor_port=Port.A, r_motor_port=Port.B,
-                l_ultra_port=Port.S1, r_ultra_port=Port.S2, 
-                wheel_diameter=55.5, axle_track=104,
-                tile_len = 230, # 23cm, longest side of the robot
-                motor_direction=Direction.COUNTERCLOCKWISE)
-#read file
-json_file = open('output2commands/position.json', 'r')
+ev3.speaker.beep()
+
+# Read positions from .json file
+json_file = open('position5.json', 'r')
 json_data = json_file.read()
-
-#parse
 obj = json.loads(json_data)
-pos_list = obj['data'] # list of positions
+positions = obj['data'] # list of positions
+print('List of positions:', positions)
 
-#Read each step and move robot
-count = 0
+# Create Robot instance
+robot = Robot(l_motor_port=Port.B, r_motor_port=Port.A,
+                l_ultra_port=Port.S2, r_ultra_port=Port.S3, 
+                wheel_diameter=55.5, axle_track=104,
+                tile_len = 300, # 30cm - tile side length
+                motor_direction=Direction.CLOCKWISE)
+
+# Some notations
+cnt = 0
 UP = 1
 DOWN = 2
 LEFT = 3
 RIGHT = 4
 head = UP # place robot with its head up
 
-for enum, i  in enumerate(pos_list):
-    print(i)
-    #get pos to go
-    curPosX = i[0]
-    curPosY = i[1]
+for pos in positions:
+    print('Cur: ', pos)
+    x, y = pos
 
-    #moving robot by curPos and prevPos
-    #Go staight 
-    # if count != 0:  #stay still at first position
-    robot.forward()
+    if cnt >= len(positions) - 1: 
+        break
+
+    next_x = positions[cnt + 1][0]
+    next_y = positions[cnt + 1][1]
+
+    if head == UP:
+        if next_x == x and next_y < y: # turn left, head points left
+            robot.turn_left()
+            head = LEFT
+        elif next_x == x and next_y > y: # turn right, head points right
+            robot.turn_right()
+            head = RIGHT
+        elif next_x > x and y == next_y: # turn back, head points downward
+            robot.turn_back()
+            head = DOWN
+        else: # case next_x < x and y == next_y: just go forward
+            pass
+
+    elif head == DOWN:
+        if next_x == x and next_y < y: # turn right, head points left
+            robot.turn_right()
+            head = LEFT
+        elif next_x == x and next_y > y: # turn left, head points right
+            robot.turn_left()
+            head = RIGHT
+        elif next_x < x and next_y == y: # turn back, head points up
+            robot.turn_back()
+            head = UP
+        else:
+            pass
+
+    elif head == LEFT:
+        if next_x == x and next_y > y: # turn back, head points right
+            robot.turn_back()
+            head = RIGHT
+        elif next_x < x and next_y == y: # turn right, head points up
+            robot.turn_right()
+            head = UP
+        elif next_x > x and next_y == y: # turn left, head points down
+            robot.turn_left()
+            head = DOWN
+        else:
+            pass
+
+    elif head == RIGHT:
+        if next_x == x and next_y < y: # turn back, head points left
+            robot.turn_back()
+            head = LEFT
+        elif next_x < x and next_y == y: # turn left, head points up
+            robot.turn_left()
+            head = UP
+        elif next_x > x and next_y == y: # turn right, head points down
+            robot.turn_right()
+            head = DOWN
+        else:
+            pass
     
-    #check next step to decide turn or not
-    if count < len(pos_list) - 1:  #last position not check anymore
-        nextPosX = pos_list[count+1][0]
-        nextPosY = pos_list[count+1][1]
-        if head == UP:
-            if nextPosX == curPosX and curPosY > nextPosY: #turn left(head left)
-                robot.turn_left()
-                head = LEFT
-            if nextPosX == curPosX and curPosY < nextPosY: #turn right(head right)
-                robot.turn_right()
-                head = RIGHT
-            if nextPosX > curPosX and curPosY == nextPosY: #turn back(head down)
-                robot.turn_back()
-                head = DOWN
-        elif head == DOWN:
-            if nextPosX == curPosX and curPosY < nextPosY: #turn left(head left)
-                robot.turn_right()
-                head = LEFT
-            if nextPosX == curPosX and curPosY > nextPosY: #turn right(head right)
-                robot.turn_left()
-                head = RIGHT
-        elif head == LEFT:
-            if nextPosY == curPosY and curPosX > nextPosX: #turn right(head up)
-                robot.turn_right()
-                head = UP
-            if nextPosY == curPosY and curPosX < nextPosX: #turn left(head down)
-                robot.turn_left()
-                head = DOWN
-        elif head == RIGHT:
-            if nextPosY == curPosY and curPosX > nextPosX: #turn left(head up)
-                robot.turn_left()
-                head = UP
-            if nextPosY == curPosY and curPosX < nextPosX: #turn right(head down)
-                robot.turn_right()
-                head = DOWN
-    count+=1
+    ev3.speaker.beep()
+    robot.forward()
+    cnt += 1
 
+ev3.speaker.beep()
