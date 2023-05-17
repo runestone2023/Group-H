dfae21f9ac835d1c84c9907e7d9faceaccf606da
Long Hoang <79366791+HeyLongHoang@users.noreply.github.com>
2023-05-17 18:10:45 +0700

    Update Robot class to support autonomous navigation

--- autonomous_navigation/output2commands/robot.py
+++ autonomous_navigation/output2commands/robot.py
@@ -58,7 +58,89 @@ class Robot(DriveBase):
     def turn_back(self):
         print("Turning back")
         self.turn(180)
+
+    def forward_stable(self):
+        ''' 
+        Robot keeps going stably between two walls until it reaches an intersection 
+        '''
+        print("Forward stable")
+        cnt = 0
+        flag = False
+        while True:
+            l, r = self.ultra_vals()
+            wait(100)
+            if l > 300 or r > 300:
+                break
+
+            elif flag or (l > 50 and r > 50): # good distance to 2 walls
+                print('Good distance')
+                cnt += 1
+                if cnt == 2:
+                    print('Good distance 2 times. Keep forward')
+                    flag = True
+
+            elif r < 50: # too close to right wall
+                print('Veer left')
+                self.turn(-20)
+                self.straight(70)
+                self.turn(20)
+
+            elif l < 50: # too close to left wall
+                print('Veer right')
+                self.turn(20)
+                self.straight(70)
+                self.turn(-20)
+
+            self.straight(100)
+
+        return l, r
+
+    def check_options(self):
+        '''
+        At the intersection, robot checks which direction it can go
+        After this, A-star decides which way to go
+        '''
+        l, r = self.ultra_vals()
+        left, right, forward = False, False, False
+        if l > 300:
+            print("Left:", l, "OK")
+            left = True
+            # More here
+        if r > 300:
+            print("Right:", r, "OK")
+            right = True
+            # More here
+
+        self.turn(-90)
+        r = self.r_ultra.distance()
+        self.turn(+90)
         
+        if r > 300:
+            print("Forward:", r, "OK")
+            forward = True
+            # More here
+        return left, right, forward
+
+    def auto(self):
+        while True:
+            left, right, forward = self.check_options()
+            print('Options (left, right, forward):', left, right, forward)
+            # Currently, the robot goes only if one of the directions is availabe
+            if not left and not right and forward:
+                self.forward_stable()
+            elif left and not right and not forward:
+                self.straight(130) 
+                self.turn_left()
+                self.straight(300) # go to the next tile after turning left
+                self.forward_stable()
+            elif not left and right and not forward:
+                self.straight(130)
+                self.turn_right()
+                self.straight(300) # go to next tile
+                self.forward_stable()
+            else:
+                print('END')
+                break  
 
         
 
