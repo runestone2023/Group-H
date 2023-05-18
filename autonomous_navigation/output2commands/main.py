4408c15fd7082301a92356be194e8ebf7ffe097b
Long Hoang <79366791+HeyLongHoang@users.noreply.github.com>
2023-05-18 12:22:09 +0700

    Add new functions to robot.py and clean up main.py

--- autonomous_navigation/output2commands/main.py
+++ autonomous_navigation/output2commands/main.py
@@ -3,102 +3,18 @@ Program to run the robot based on the output of A-star algorithm
 Need to convert the output from type .csv to type .json
 '''
 
-#!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-
-import json
 # from robot import *
 from output2commands.robot import *
-
-ev3 = EV3Brick()
-ev3.speaker.beep()
-
-# Read positions from .json file
-json_file = open('position5.json', 'r')
-json_data = json_file.read()
-obj = json.loads(json_data)
-positions = obj['data'] # list of positions
-print('List of positions:', positions)
 
 # Create Robot instance
 robot = Robot(l_motor_port=Port.B, r_motor_port=Port.A,
                 l_ultra_port=Port.S2, r_ultra_port=Port.S3, 
+                color_port=Port.S1, tile_len = 300, # 30cm - tile side length
                 wheel_diameter=55.5, axle_track=104,
-                tile_len = 300, # 30cm - tile side length
                 motor_direction=Direction.CLOCKWISE)
 
-# Some notations
-cnt = 0
-UP = 1
-DOWN = 2
-LEFT = 3
-RIGHT = 4
-head = UP # place robot with its head up
+# PROGRAM
+# robot.run_path('position5.json')
+robot.auto_single_path()
 
-for pos in positions:
-    print('Cur: ', pos)
-    x, y = pos
 
-    if cnt >= len(positions) - 1: 
-        break
-
-    next_x = positions[cnt + 1][0]
-    next_y = positions[cnt + 1][1]
-
-    if head == UP:
-        if next_x == x and next_y < y: # turn left, head points left
-            robot.turn_left()
-            head = LEFT
-        elif next_x == x and next_y > y: # turn right, head points right
-            robot.turn_right()
-            head = RIGHT
-        elif next_x > x and y == next_y: # turn back, head points downward
-            robot.turn_back()
-            head = DOWN
-        else: # case next_x < x and y == next_y: just go forward
-            pass
-
-    elif head == DOWN:
-        if next_x == x and next_y < y: # turn right, head points left
-            robot.turn_right()
-            head = LEFT
-        elif next_x == x and next_y > y: # turn left, head points right
-            robot.turn_left()
-            head = RIGHT
-        elif next_x < x and next_y == y: # turn back, head points up
-            robot.turn_back()
-            head = UP
-        else:
-            pass
-
-    elif head == LEFT:
-        if next_x == x and next_y > y: # turn back, head points right
-            robot.turn_back()
-            head = RIGHT
-        elif next_x < x and next_y == y: # turn right, head points up
-            robot.turn_right()
-            head = UP
-        elif next_x > x and next_y == y: # turn left, head points down
-            robot.turn_left()
-            head = DOWN
-        else:
-            pass
-
-    elif head == RIGHT:
-        if next_x == x and next_y < y: # turn back, head points left
-            robot.turn_back()
-            head = LEFT
-        elif next_x < x and next_y == y: # turn left, head points up
-            robot.turn_left()
-            head = UP
-        elif next_x > x and next_y == y: # turn right, head points down
-            robot.turn_right()
-            head = DOWN
-        else:
-            pass
-    
-    ev3.speaker.beep()
-    robot.forward()
-    cnt += 1
-
-ev3.speaker.beep()
--- autonomous_navigation/output2commands/robot.py
+++ autonomous_navigation/output2commands/robot.py
@@ -6,12 +6,14 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
+import json
+
 class Robot(DriveBase):
     '''Wrapper class for class DriveBase'''
     def __init__(self, l_motor_port, r_motor_port, 
                     l_ultra_port, r_ultra_port,
+                    color_port, tile_len,
                     wheel_diameter, axle_track, 
-                    tile_len,
                     motor_direction=Direction.CLOCKWISE):
         '''
         Args:
@@ -19,17 +21,20 @@ class Robot(DriveBase):
         r_motor_port - Port that right motor is plugged e.g. Port.D
         l_ultra_port - Port that left ultrasonic sensor is plugged e.g. Port.S1
         r_ultra_port - Port that right ultrasonic sensor is plugged e.g. Port.S1
+        color_port - Port that color sensor is plugged
         wheel_diameter - diameter of motor wheels
         axle_track - distance between two motor wheels
         motor_direction - Direction of motor, default value is 'clockwise'
         tile_len - Length of a side of the tile, 
-                    which is equal to the length of the longest side of the robot
+                    which is equal to the length of the longest side of the self
         '''
         self.l_motor = Motor(l_motor_port, positive_direction=motor_direction)
         self.r_motor = Motor(r_motor_port, positive_direction=motor_direction)
         super().__init__(self.l_motor, self.r_motor, wheel_diameter, axle_track)
         self.l_ultra = UltrasonicSensor(l_ultra_port)
         self.r_ultra = UltrasonicSensor(r_ultra_port)
+        self.color = ColorSensor(color_port)
+        self.ev3 = EV3Brick()
         self.tile_len = tile_len
 
     def ultra_vals(self):
@@ -39,24 +44,26 @@ class Robot(DriveBase):
         print("Left:", l, " Right:", r)
         return l, r
 
+    def color_val(self):
+        ''' Get value of color sensor '''
+        color = self.color.color()
+        print('Color: ', color)
+        return color
+
     def forward(self):
-        print("Going forward")
+        print("Go forward")
         self.straight(self.tile_len) 
 
-    def backward(self):
-        print("Going backward")
-        self.straight(-1 * self.tile_len) 
-
     def turn_right(self):
-        print("Turning right")
+        print("Turn right")
         self.turn(90)
 
     def turn_left(self):
-        print("Turning left")
+        print("Turn left")
         self.turn(-90)
 
     def turn_back(self):
-        print("Turning back")
+        print("Turn back")
         self.turn(180)
 
     def forward_stable(self):
@@ -121,26 +128,114 @@ class Robot(DriveBase):
             # More here
         return left, right, forward
 
-    def auto(self):
+    def auto_single_path(self):
+        ''' The robot finds its way to the end of a single path '''
+        self.ev3.speaker.beep()
         while True:
             left, right, forward = self.check_options()
+            color = self.color_val()
             print('Options (left, right, forward):', left, right, forward)
             # Currently, the robot goes only if one of the directions is availabe
             if not left and not right and forward:
-                self.forward_stable()
+                pass
             elif left and not right and not forward:
-                self.straight(130) 
                 self.turn_left()
-                self.straight(300) # go to the next tile after turning left
-                self.forward_stable()
             elif not left and right and not forward:
-                self.straight(130)
                 self.turn_right()
-                self.straight(300) # go to next tile
-                self.forward_stable()
+            elif not left and not right and not forward and color != Color.GREEN:
+                self.turn_back()
             else:
-                print('END')
-                break  
-
+                self.turn_back()
+                break
+            self.forward()
         
+        self.ev3.speaker.beep()
+        print('END')
 
+    def run_path(self, position_file):
+        '''
+        Args:
+        position_file - path to .json file that store the path (output of A-star)
+        '''
+        # Read positions from .json file
+        json_file = open(position_file, 'r')
+        json_data = json_file.read()
+        obj = json.loads(json_data)
+        positions = obj['data'] # list of positions
+        print('List of positions:', positions)
+
+        # Some notations
+        cnt = 0
+        UP = 1
+        DOWN = 2
+        LEFT = 3
+        RIGHT = 4
+        head = UP # place robot with its head up
+
+        for pos in positions:
+            print('Cur: ', pos)
+            x, y = pos
+
+            if cnt >= len(positions) - 1: 
+                break
+
+            next_x = positions[cnt + 1][0]
+            next_y = positions[cnt + 1][1]
+
+            if head == UP:
+                if next_x == x and next_y < y: # turn left, head points left
+                    self.turn_left()
+                    head = LEFT
+                elif next_x == x and next_y > y: # turn right, head points right
+                    self.turn_right()
+                    head = RIGHT
+                elif next_x > x and y == next_y: # turn back, head points downward
+                    self.turn_back()
+                    head = DOWN
+                else: # case next_x < x and y == next_y: just go forward
+                    pass
+
+            elif head == DOWN:
+                if next_x == x and next_y < y: # turn right, head points left
+                    self.turn_right()
+                    head = LEFT
+                elif next_x == x and next_y > y: # turn left, head points right
+                    self.turn_left()
+                    head = RIGHT
+                elif next_x < x and next_y == y: # turn back, head points up
+                    self.turn_back()
+                    head = UP
+                else:
+                    pass
+
+            elif head == LEFT:
+                if next_x == x and next_y > y: # turn back, head points right
+                    self.turn_back()
+                    head = RIGHT
+                elif next_x < x and next_y == y: # turn right, head points up
+                    self.turn_right()
+                    head = UP
+                elif next_x > x and next_y == y: # turn left, head points down
+                    self.turn_left()
+                    head = DOWN
+                else:
+                    pass
+
+            elif head == RIGHT:
+                if next_x == x and next_y < y: # turn back, head points left
+                    self.turn_back()
+                    head = LEFT
+                elif next_x < x and next_y == y: # turn left, head points up
+                    self.turn_left()
+                    head = UP
+                elif next_x > x and next_y == y: # turn right, head points down
+                    self.turn_right()
+                    head = DOWN
+                else:
+                    pass
+            
+            self.ev3.speaker.beep()
+            self.forward()
+            cnt += 1
+
+        self.ev3.speaker.beep()
