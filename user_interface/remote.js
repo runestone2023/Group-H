28d4c91fdde544e5d2e283b32b352a22363f887f
Darian Krummrich <d.krummrich@gmx.net>
2023-05-18 17:07:33 +0200

    changed remote.html in user_interface to include all necessary content for video conferences, modified it to have control panel in fixed position to allow more space for videos

--- user_interface/remote.js
+++ user_interface/remote.js
@@ -1,4 +1,8 @@
 // --------- handles different input types keyboard and mouse, calls integration with robot control ----------
+let default_mode = "manual";
+let current_mode = null;
+let is_pathing = false;
+
 
 // handles retrieving input from the slider and reflecting it back to the view
 function sliderUpdate(){
@@ -33,7 +37,7 @@ function keyPressInput(event) {
 function keyReleaseRecolour(event){
     let c = document.getElementsByClassName("button");
     for (let i=0; i<c.length; i++) {
-        if (c[i].id != "s_button"){
+        if (c[i].id != "s_button" && c[i].id != "m_button"){
             c[i].style.backgroundColor = "#0a5b68b4";
         }
     }
@@ -44,18 +48,39 @@ function keyReleaseRecolour(event){
 // when a button is held down by mouse, colour it lighter and start movement in the corresponding direction
 function buttonPressInput(buttonID, direction){
     document.getElementById(buttonID).style.backgroundColor = "rgba(6, 51, 56, 0.71)";
+
     if(direction=="autonomous" || direction=="manual"){
+        if(direction=="autonomous"){
+            document.getElementById("m_button").style.backgroundColor = "#0a5b68b4";
+        } else if (direction == "manual"){
+            document.getElementById("a_button").style.backgroundColor = "#0a5b68b4";
+        }
         switch_control_mode(direction);
-    } else if (direction=="path"){
+    } else if (direction=="path" && !is_pathing){
+        document.getElementById("m_button").style.backgroundColor = "#0a5b68b4";
+        document.getElementById("a_button").style.backgroundColor = "#0a5b68b4";
         find_path();
+        is_pathing = true;
+    } else if (direction=="path" && is_pathing){
+        abortPathing();
+        is_pathing = false;
     }
     button_direction(direction);
 }
 
 // when a button is released by mouse, colour it darker and stop movement
 function buttonReleaseInput(buttonID){
-    document.getElementById(buttonID).style.backgroundColor = "#0a5b68b4";
-    button_direction('s');
+    if(buttonID !== 'a_button' &&  buttonID !== 'm_button' && buttonID !== 'best_button'){
+        document.getElementById(buttonID).style.backgroundColor = "#0a5b68b4";
+        button_direction('s');
+    }
+
+}
+
+function abortPathing(){
+    switch_control_mode("manual");
+    document.getElementById("m_button").style.backgroundColor = "rgba(6, 51, 56, 0.71)";
+    document.getElementById("best_button").style.backgroundColor = "#0a5b68b4";
 }
 
 
@@ -75,7 +100,19 @@ function speed_slider(new_speed){
 
 // toggle manual / autonomous control (default should be control)
 function switch_control_mode(new_mode){
+    current_mode = new_mode;
     document.getElementById("control_mode_demo").innerHTML = new_mode;
+
+}
+
+function get_control_mode(){
+    if (current_mode) {
+        document.getElementById("test_default_mode").innerHTML = current_mode;
+        return current_mode;
+    } else {
+        document.getElementById("test_default_mode").innerHTML = default_mode;
+        return default_mode;
+    }
 }
 
 // execute AStar
